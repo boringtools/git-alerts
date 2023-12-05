ccf6e01dbcf799a22d945f7b7ec13957c12aaae0
c0d3g33k <nikhil.mittal641@gmail.com>
2023-12-05 20:36:49 +0530

    Added support for secret scanning

--- cmd/detect.go
+++ cmd/detect.go
@@ -5,36 +5,31 @@ import (
 
 	"github.com/boringtools/git-alerts/common"
 	"github.com/boringtools/git-alerts/gh"
-
 	"github.com/boringtools/git-alerts/logger"
-	"github.com/boringtools/git-alerts/reporter"
+	"github.com/boringtools/git-alerts/secrets"
 	"github.com/spf13/cobra"
 )
 
-var monitorCmd = &cobra.Command{
-	Use:   "monitor",
-	Short: "Monitor public repositories",
+var detectCmd = &cobra.Command{
+	Use:   "detect",
+	Short: "Scan with secrets detection",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		envs := map[string]string{
 			"org":     org,
 			"rfp":     report,
 			"command": cmd.Use,
 			"csv":     strconv.FormatBool(csv),
-			"slack":   strconv.FormatBool(slack),
 		}
-
 		common.SetEnvs(envs)
 
-		common.CheckScanFiles()
 		gh.Connecter()
-		reporter.Notify()
+		secrets.GetSecrets()
+
 		logger.LogP("Scan ended : ", common.GetTime())
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(monitorCmd)
-	monitorCmd.PersistentFlags().BoolVarP(&slack, "slack-alert", "s", false, "Slack notification")
+	rootCmd.AddCommand(detectCmd)
 }
